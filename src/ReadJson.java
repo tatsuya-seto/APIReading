f8f2bbd9d38b823261a9cdf2c4be1177824ee02f
tatsuya-seto <tatsuya_seto28@milton.edu>
2025-12-03 11:14:08 -0500

    Initial commit

---
+++ src/ReadJson.java
@@ -0,0 +1,109 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+// video to load jar
+//https://www.youtube.com/watch?v=QAJ09o3Xl_0
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+// Program for print data in JSON format.
+public class ReadJson {
+    public static void main(String args[]) throws ParseException {
+        // In java JSONObject is used to create JSON object
+        // which is a subclass of java.util.HashMap.
+
+        JSONObject file = new JSONObject();
+        file.put("Full Name", "Ritu Sharma");
+        file.put("Roll No.", 1704310046);
+        file.put("Tution Fees", 65400);
+
+
+        // To print in JSON format.
+        //System.out.print(file.get("Tution Fees"));
+        ReadJson readingIsWhat = new ReadJson();
+
+    }
+
+    public ReadJson(){
+        try {
+            pull();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public  void pull() throws ParseException {
+        String output = "abc";
+        String totlaJson="";
+        try {
+
+            URL url = new URL("https://pokeapi.co/api/v2/pokemon/ditto");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            //if you get the error 403, uncomment line of code below
+            //conn.setRequestProperty("User-Agent", "Mozilla/5.0"); // Add User-Agent
+
+
+            if (conn.getResponseCode() != 200) {
+
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+
+            //System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                //System.out.println(output);
+                totlaJson+=output;
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        JSONParser parser = new JSONParser();
+        //System.out.println(str);
+        org.json.simple.JSONObject jsonObject = (org.json.simple.JSONObject) parser.parse(totlaJson);
+        //System.out.println(jsonObject);
+
+        try {
+
+            org.json.simple.JSONObject abilities = (org.json.simple.JSONObject) jsonObject.get("abilities");
+            int n =   abilities.size();
+            for (int i = 0; i < n; ++i) {
+                String Starships =(String) msg.get(i);
+                System.out.println(Starships);
+                // System.out.println(person.getInt("key"));
+            }
+
+            String height= (String)jsonObject.get("abilities");
+            System.out.println(height);
+
+
+        }
+
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
+    }
+}
